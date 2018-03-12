e96a1c609f74237244af1d669af4a9141fa90f57
Hiruna Jayamanne <hirunav2@gmail.com>
2018-03-12 23:27:42 +1300

    Added the ability to get info about the newest release from github.

---
+++ src/github_api.py
@@ -0,0 +1,21 @@
+from requests import get
+from json import loads
+
+class Release():
+    def __init__(self, response):
+        self.id = response["id"]
+        self.tag_name = response["tag_name"]
+        self.url = response["html_url"]
+    def __str__(self):
+        return f"Id: {self.id}\nVersion: {self.tag_name}\nURL: {self.url}"
+
+
+def get_newest_release(path):
+    URL = f"https://api.github.com/repos/{path}/releases/latest"
+    request = get(URL)
+    newest_release = Release(request.json())
+    return newest_release
+
+if __name__ == "__main__":
+    x = get_newest_release("HiruNya/Overwatch-Checklist-rs")
+    print(x)
